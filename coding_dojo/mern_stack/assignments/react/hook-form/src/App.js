c36cba9c9666e8d73cdd8e6beaaf9108bea1df4d
ctmckeel93 <74134095+ctmckeel93@users.noreply.github.com>
2021-01-08 17:08:51 -0500

    Add files via upload

---
+++ coding_dojo/mern_stack/assignments/react/hook-form/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState} from 'react'
+import './App.css';
+import UserForm  from './components/forms'
+
+
+function App() {
+
+  return (
+    <>
+    <div className="App">
+    <UserForm />
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ coding_dojo/mern_stack/assignments/react/hook-form/src/components/forms.jsx
@@ -0,0 +1,181 @@
+import React, { useState } from 'react'
+
+const UserForm = props => {
+	const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+	const [passwordConfirm, setPasswordConfirm] = useState("")
+	const [firstNameError, setFirstNameError] = useState("")
+	const [lastNameError, setLastNameError] = useState("")
+	const [emailError, setEmailError] = useState("")
+	const [passwordError, setPasswordError] = useState("")
+	const [passwordConfirmError, setPasswordConfirmError] = useState("")
+	
+	
+		
+		const fName = firstName
+		const lName = lastName
+		const emailAddress = email
+		const pwd = password
+		const cp = passwordConfirm
+		// const initialState = {
+		// 	firstName: {
+		// 		value: '',
+		// 		error: null
+		// 	},
+		// 	lastName: {
+		// 		value: '',
+		// 		error: null
+		// 	},
+		// 	email: {
+		// 		value: '',
+		// 		error: null
+		// 	},
+		// 	password: {
+		// 		value: '',
+		// 		error: null
+		// 	},
+		// 	passConfirm: {
+		// 		value: '',
+		// 		error: null
+		// 	},
+
+		// }
+
+		const handleFirstName = (e) => {
+			
+			setFirstName(e.target.value)
+
+				if (e.target.value.length < 2 && e.target.value.length !== 0){
+					setFirstNameError("First Name must be at least 2 characters")
+				}
+				else {
+					setFirstNameError('')
+				}
+				console.log(setFirstName)
+		}
+
+		const handleLastName = (e) => {
+			
+			setLastName(e.target.value)
+
+				if (e.target.value.length < 2 && e.target.value.length !== 0){
+					setLastNameError("Last Name must be at least 2 characters")
+				}
+				else {
+					setLastNameError('')
+				}
+			
+		}
+
+		const handleEmail = (e) => {
+			
+			setEmail(e.target.value)
+			
+				if (e.target.value.length < 2 && e.target.value.length !== 0){
+					setEmailError("Email must be at least 2 characters")
+				}
+				else {
+					setEmailError('')
+				}
+		}
+		
+
+		const handlePassword = (e) => {
+			
+			setPassword(e.target.value)
+			
+				if (e.target.value.length < 8 && e.target.value.length !== 0){
+					setPasswordError("Password must be at least 8 characters")
+				}
+				else {
+					setPasswordError('')
+				}
+			
+		}
+
+		const handlePasswordConfirm = (e) => {
+			
+			setPasswordConfirm (e.target.value)
+			
+				console.log("Password Confirm: " + e.target.value)
+				console.log("Password: " + pwd)
+				if (e.target.value !== pwd && e.target.value.length !== 0){
+					setPasswordConfirmError("Passwords must match!")
+				}
+				else {
+					setPasswordConfirmError('')
+				}
+			
+		}
+			
+		
+	
+		const createUser = e => {
+			e.preventDefault();
+			const newUser = { firstName, lastName, email, password, passwordConfirm };
+			console.log("Welcome", newUser)
+			setFirstName("")
+			setLastName("")
+			setEmail("")
+			setPassword("")
+			setPasswordConfirm("")
+		}
+		return(
+			<>
+			<form onSubmit={createUser}>
+					<div>
+						<label>First name: </label>
+						<input type="text" onChange={ handleFirstName }  value={ firstName }/>
+					</div>
+
+					
+					<p className="error">{firstNameError}</p>
+
+					<div>
+						<label>Last Name: </label>
+						<input type="text" onChange={ handleLastName} value={lastName}/>
+					</div>
+
+
+					<p className="error">{lastNameError}</p>
+
+					<div>
+						<label>Email: </label>
+						<input type="text" onChange={ handleEmail } value={email}/>
+					</div>
+					
+
+					<p className="error">{emailError}</p>
+					<div>
+						<label>Password: </label>
+						<input type="text" onChange={ handlePassword } value={password}/>
+					</div>
+					
+
+					<p className="error">{passwordError}</p>
+					<div>
+						<label>Confirm Password: </label>
+						<input type="text" onChange={ handlePasswordConfirm } value={passwordConfirm}/>
+					</div>
+
+					<p className="error">{passwordConfirmError}</p>
+					<input type="submit" value="Create User" />
+				</form>
+
+				<p>First Name: {fName}</p>
+				<p>Last Name: {lName}</p>
+				<p>Email: {emailAddress}</p>
+				<p>Password: {pwd}</p>
+				<p>Confirm Password: {cp} </p>
+			</>
+
+			
+			
+	)
+	
+	
+}
+
+export default UserForm
